61bd61003149800233627a941e7e6822cfb4ab3b
Saurabh Gattani <saurabh.gattani@gmail.com>
2018-07-16 07:41:52 +1000

    code improvements for initiliazing the chevron show/hide flag array in a generic way instead of hard coding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,27 +12,32 @@ import {
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.css']
 })
+
+/* This is the root AppComponent class */
 export class AppComponent implements OnInit {
 	title = 'app';
 	busSchedules;
 	chevronToggle;
 	
 	
-
+	/* constructor of the component */
 	constructor(private busServices: BusService) {
 		this.busSchedules = null;
-		this.chevronToggle = [0, 0];
+		this.chevronToggle = [];
 	}
 
+	/* extract and return route ID which is shown in bold */
 	public getRouteId(route: string) {
 		return route.split(" ")[0];
 	}
 	
+	/* extract and return all but first 3 digits from the routeVariant attribute string */
 	public getAllButRouteId(route: string) {
 		let r = " "+ route.split(" ").slice(1).join(" ");
 		return r;
 	}
 	
+	/* resets all but the current open panel which closes previously open panels */
 	private resetToggle(index) {
 		let carr = this.chevronToggle;
 		for (let i = 0; i < carr.length; i++) {
@@ -42,6 +47,15 @@ export class AppComponent implements OnInit {
 		}
 	}
 	
+	/* Initializes chevronToggle array after receiving service response */
+	private initChevron() {
+		let busServices = this.busSchedules.data;
+		for (let i = 0; i < busServices.length; i++) {
+			this.chevronToggle.push(0);
+		}
+	}
+
+	/* Returns bus timing status */
 	public getScheduleStatus(timeDiff: number) {
 		if (timeDiff < 0) {
 			return 'Late';
@@ -52,16 +66,22 @@ export class AppComponent implements OnInit {
 		}
 	}
 	
+	/* checks if bus time deviation status is unknown */
 	public isUnknown(timeDiff: object) {
 		return timeDiff === null;
 	}
 	
+	/* toggles the bus info panel */
 	public togglePanel(event, index) {
 		this.chevronToggle[index] = this.chevronToggle[index] ^ 1;
 		this.resetToggle(index);
 	}
 	
+	/* default ngOnInit angular function */
 	ngOnInit() {
-		this.busServices.getBusSchedules().subscribe(p => this.busSchedules = p);
+		this.busServices.getBusSchedules().subscribe(p => {
+			this.busSchedules = p;
+			this.initChevron();
+		});
 	}
 }
